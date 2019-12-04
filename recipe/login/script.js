188ddbb55385121347a08a87e7a458e26695f06c
Angel Karafas <akarafas@live.unc.edu>
2019-12-04 17:00:33 -0500

    getting basic html

--- recipe/login/script.js
+++ recipe/login/script.js
@@ -1,30 +1,81 @@
+const renderHomePage = function() {
+  $('#root').on("click", "#back_button", handleBackButton);
+  return `<div>
+        <section class="hero is-bold is-dark">
+          <div class="hero-body">
+            <div class="container">
+              <h1 class="title">
+                Recipe Book
+              </h1>
+              <h2 class="subtitle">
+                COMP426
+              </h2>
+            </div>
+            <div class="section buttons">
+              <button id="back_button" class="button is-link is-light">Back</button>
+            </div>
+          </div>
+        </section>
+        
+      </div>
+      <div class="container is-fluid">
+        <div class="notification">
+          place login stuff?
+        </div>
+      </div>`
+};
+
+const handleBackButton = function (event) {
+  event.preventDefault();
+  event.stopImmediatePropagation();
+  location.href=`../index.html`;
+
+};
+
+
+
+
+const loadDom = function() {
+  const $root = $('#root');
+  $root.append(renderHomePage());
+};
+
 $(function() {
-    const $form = $('#login-form');
-    const $message = $('#message');
+    async function main() {
+        loadDom();
+    }
+    main();
+});
+
+
+
+// $(function() {
+//     const $form = $('#login-form');
+//     const $message = $('#message');
   
-    $form.submit(function(e) {
-      e.preventDefault();
+//     $form.submit(function(e) {
+//       e.preventDefault();
   
-      $message.html('');
+//       $message.html('');
   
-      const data = $form.serializeArray().reduce((o, x) => {
-        o[x.name] = x.value;
-        return o;
-      }, {});
+//       const data = $form.serializeArray().reduce((o, x) => {
+//         o[x.name] = x.value;
+//         return o;
+//       }, {});
       
-      $.ajax({
-        url: 'https://comp426fa19.cs.unc.edu/sessions/login',
-        type: 'POST',
-        data,
-        xhrFields: {
-            withCredentials: true,
-        },
-      }).then(() => {
-        $message.html('<span class="has-text-success">Success! You are now logged in.</span>');
-        // redirect
-        location.href=`../index.html`;
-      }).catch(() => {
-        $message.html('<span class="has-text-danger">Something went wrong and you were not logged in. Check your email and password and your internet connection.</span>');
-      });
-    });
-  });
+//       $.ajax({
+//         url: 'https://comp426fa19.cs.unc.edu/sessions/login',
+//         type: 'POST',
+//         data,
+//         xhrFields: {
+//             withCredentials: true,
+//         },
+//       }).then(() => {
+//         $message.html('<span class="has-text-success">Success! You are now logged in.</span>');
+//         // redirect
+//         location.href=`../index.html`;
+//       }).catch(() => {
+//         $message.html('<span class="has-text-danger">Something went wrong and you were not logged in. Check your email and password and your internet connection.</span>');
+//       });
+//     });
+//   });
--- recipe/script.js
+++ recipe/script.js
@@ -1,36 +1,50 @@
 const renderHomePage = function() {
     $('#root').on("click", "#login_button", handleLoginButton);
     $('#root').on("click", "#signup_button", handleSignupButton);
-    return `<div class="buttons">
-    <button class="button is-link is-light">Login</button>
-    <button class="button is-link is-light">Sign Up</button>
-</div>`
-
+    return `<div>
+          <section class="hero is-bold is-dark">
+            <div class="hero-body">
+              <div class="container">
+                <h1 class="title">
+                  Recipe Book
+                </h1>
+                <h2 class="subtitle">
+                  COMP426
+                </h2>
+              </div>
+              <div class="section buttons">
+                <button id="login_button" class="button is-link is-light">Login</button>
+                <button id="singup_button" class="button is-link is-light">Sign Up</button>
+              </div>
+            </div>
+          </section>
+          <div class="tabs is-medium">
+            <ul>
+              <li class="is-active"><a>Recipes</a></li>
+              <li><a>Planning</a></li>
+              <li><a>Find store</a></li>
+              <li><a>Find recipes</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="container is-fluid">
+          <div class="notification">
+            render recipe feed here!!!
+          </div>
+        </div>`
   };
 
-const handleSignupButton = function (event) {
+const handleLoginButton = function (event) {
     event.preventDefault();
     event.stopImmediatePropagation();
-
+    location.href=`../login/index.html`;
 
 };
 
 const handleSignupButton = function (event) {
     event.preventDefault();
-    event.stopImmediatePropagation();
-  
-    async function updateTweet() {
-      const result = await axios({
-        method: 'put',
-        url: `https://comp426fa19.cs.unc.edu/a09/tweets/${event.target.parentElement.id}`,
-        withCredentials: true,
-        data: {
-          body: tweet,
-        },
-      });
-      $(`.${event.target.parentElement.id}`).replaceWith(renderMyTweets(result.data));
-    }
-    updateTweet();
+    event.stopImmediatePropagation();  
+    location.href=`../create_account/index.html`;
   };
 
 
@@ -47,4 +61,3 @@ const loadDom = function() {
       }
       main();
   });
-  console.log("hi");
