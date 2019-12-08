ee26dfc9198454d357cbc7c8f50ee3786ce18ec1
Angel Karafas <akarafas@live.unc.edu>
2019-12-07 23:24:59 -0500

    motivation finished

--- recipe/motiv/script.js
+++ recipe/motiv/script.js
@@ -31,8 +31,9 @@ const renderPrivateHomePage = function() {
           </div>
           <div class="container is-fluid">
           <div class="notification">
-            <h1 class="title is-4"> " "</h1>
-            <div class="place_motive">Will it replace</div>
+            <h1 class="title is-4">Press the button to get motivation!</h1>
+            <div id="place_pic"></div>
+            <div id="place_motive"></div>
             <button id="gen_button" class="button is-primary">Generate</button>
           </div>
         </div>
@@ -65,21 +66,43 @@ const handleStoreButton = function (event) {
   location.href=`../../mapquest_api/index.html`;
 };
 
+const createPic = function () {
+  let arr = [`<img class="place_pic" src="https://cdn.shopify.com/s/files/1/0393/0833/products/4599_2F1461180668_2Fimg_5934_720x.jpeg?v=1461180882" alt="Remmy">`,
+  `<img class="place_pic" src="https://usercontent1.hubstatic.com/13161162_f520.jpg" alt="Food Wars">`,
+  `<img class="place_pic" src="  https://media1.tenor.com/images/7bda6950bdfe5dc54e02771b44043540/tenor.gif?itemid=10773063
+  " alt="Food Wars">`]
+  
+  //  "You got it chef!",
+  //  "Keep it up in the kitchen!"];
+  let num = Math.floor(arr.length*Math.random());
+  let str = arr[num];
+  return str;
+};
+
 const createMotiv = function () { 
-    let arr = ["Keep cooking!", "Go Chef!", "Chef on!"];
-    let num = Math.floor(arr.length*Math.random());
-    let str = arr[num];
-    console.log(str);
-    // $("place_motiv").append(`<div>`+str+`</div>`);
-    return `<div>` + str + '</div>'
+  let arr = ["Keep cooking!", "Go Chef!", "Don't cry, it's just an onion!", "Just keep cooking!", "Cook on!", "You got it chef!", "Keep it up in the kitchen!", "Just add butter!"];
+  let picarr = [`<img class="place_pic" src="https://cdn.shopify.com/s/files/1/0393/0833/products/4599_2F1461180668_2Fimg_5934_720x.jpeg?v=1461180882" alt="Remmy">`,
+  `<img class="place_pic is-square" src="https://usercontent1.hubstatic.com/13161162_f520.jpg" alt="Food Wars">`,
+  `<img class="place_pic is-square" src="https://media1.tenor.com/images/7bda6950bdfe5dc54e02771b44043540/tenor.gif?itemid=10773063" alt="Spirited Away">`,
+  `<img class="place_pic is-square" src="https://i.guim.co.uk/img/static/sys-images/Guardian/Pix/pictures/2015/3/4/1425486699559/Mama-Cook-007.jpg?width=300&quality=85&auto=format&fit=max&s=342348b81538cca5230913ff67aac525" alt="Cooking Mama">`,
+  `<img class="place_pic image" src="https://cdn.vox-cdn.com/thumbor/b708MoUXEgWxcKVNUpl5fibuuQc=/0x0:4096x2304/1200x800/filters:focal(1721x825:2375x1479)/cdn.vox-cdn.com/uploads/chorus_image/image/59497417/BAO_RGB_s110_19c.pub16.172.0.jpg" alt="Bao" width="480" height="480">`,
+  `<img class="place_pic is-square" src="https://thumbs.gfycat.com/AcclaimedDependableAssassinbug-max-1mb.gif" alt="SpongeBob">`,
+  `<img class="place_pic is-square" src="https://media.giphy.com/media/RJ1hYRDhaD3yHUXG4u/giphy.gif" alt="Wendys">`,
+  `<img class="place_pic is-square" src="https://media2.giphy.com/media/l0Ex5cj3jF05FTVXq/giphy.gif" alt="Pretzel">`];
+  let num = Math.floor(arr.length*Math.random());
+  let str = arr[num];
+  let picstr = picarr[num];
+  return `<div id="place_motive" class="title is-4 has-text-primary">` + `<div class="section">` + str + `</div>` + picstr + '</div>'
 };
 
 const handleGenButton = function (event) {
   event.preventDefault();
   event.stopImmediatePropagation(); 
-  // $('#place_motiv').empty();
-  // $('#place_motiv').append(createMotiv());
-  $('#place_motiv').replaceWith(createMotiv());
+  // $('#place_pic').replaceWith(createPic());
+  $('#place_motive').replaceWith(createMotiv());
+  $('#gen_button').on("click", handleGenButton);
+  // $('#place_pic').on("click", handleGenButton);
+
 };
 
 
@@ -88,9 +111,9 @@ const loadDom = function() {
   $root.append(renderPrivateHomePage());
 };
   
-  $(function() {
-      async function main() {
-          loadDom();
-      }
-      main();
-  });
+$(function() {
+  async function main() {
+    loadDom();
+  }
+    main();
+});
